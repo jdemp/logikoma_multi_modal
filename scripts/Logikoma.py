49a6811edc0f90c4b5db22a12e4e4550ab339014
Jensen Dempsey <jdemp@jensendempsey.com>
2017-04-20 08:54:37 -0600

    added logikoma control

---
+++ scripts/Logikoma.py
@@ -0,0 +1,17 @@
+import rospy
+
+class Logikoma:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def turn(self):
+        pass
+    
+
+if __name__ == '__main__':
+    rospy.init_node('Logikoma')
+    n = Logikoma()
+    n.run()
