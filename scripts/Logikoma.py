b70ef1980c66e6d65b723b7367c29a05fd2fdc56
Jensen Dempsey <jdemp@jensendempsey.com>
2017-04-24 13:58:04 -0600

    added more action processing

--- scripts/Logikoma.py
+++ scripts/Logikoma.py
@@ -4,16 +4,22 @@ import actionlib
 from actionlib_msgs.msg import *
 from logikoma_multi_modal.msg import action_output
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose
-import tf
+from geometry_msgs.msg import Pose, Point, Quaternion
+from tf import transformations
+import math
 
 
-def euler_to_quat(self, euler):
-    return tf.transformations.quaternion_from_euler(euler.x, euler.y, euler.z)
+def euler_to_quat(roll,pitch,yaw):
+    return transformations.quaternion_from_euler(roll,pitch,yaw)
 
 
-def quat_to_euler(self,quat):
-    return tf.transformations.euler_from_quaternion(quat)
+def quat_to_euler(quat):
+    return transformations.euler_from_quaternion(quat)
+
+
+def rotate_around_z(degrees, quat):
+    euler = quat_to_euler(quat)
+    return euler_to_quat(euler[0],euler[1],math.radians(degrees))
 
 
 class Logikoma:
@@ -24,27 +30,55 @@ class Logikoma:
         self.stopped = True
 
     def go_forward(self, action):
-        success = True
-        while success:
-            goal = MoveBaseGoal()
-            goal.target_pose.header.frame_id = 'base_link'
-            goal.target_pose.header.stamp = rospy.Time.now()
-            goal.target_pose.pose.position.x = self.pose.position.x +1
-            goal.target_pose.pose.position.y = self.pose.position.y
-            goal.target_pose.pose.position.z = self.pose.position.z
-            goal.target_pose.pose.orientation = self.pose.orientation
-            self.move_base.send_goal(goal)
-            success = self.move_base.wait_for_result(rospy.Duration(15))
-        self.stop()
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.pose.position.x + action
+        goal.target_pose.pose.position.y = self.pose.position.y
+        goal.target_pose.pose.position.z = self.pose.position.z
+        goal.target_pose.pose.orientation = self.pose.orientation
+        self.move_base.send_goal(goal)
+        success = self.move_base.wait_for_result(rospy.Duration(15))
+        return success
 
     def navigate_to_point(self, x, y):
-        pass
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        goal.target_pose.pose.position.z = self.pose.position.z
+        goal.target_pose.pose.orientation = self.pose.orientation
+        self.move_base.send_goal(goal)
+        success = self.move_base.wait_for_result(rospy.Duration(30))
+        return success
 
     def go_back(self):
-        pass
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.pose.position.x - 1
+        goal.target_pose.pose.position.y = self.pose.position.y
+        goal.target_pose.pose.position.z = self.pose.position.z
+        goal.target_pose.pose.orientation = self.pose.orientation
+        self.move_base.send_goal(goal)
+        success = self.move_base.wait_for_result(rospy.Duration(15))
+        return success
 
     def turn(self, action):
-        pass
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'base_link'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.pose.position.x + 0.5
+        if action == 'left':
+            goal.target_pose.pose.position.y = self.pose.position.y - 0.5
+            goal.target_pose.pose.orientation = rotate_around_z(-90,self.pose.orientation)
+        else:
+            goal.target_pose.pose.position.y = self.pose.position.y + .5
+            goal.target_pose.pose.orientation = rotate_around_z(90,self.pose.orientation)
+        self.move_base.send_goal(goal)
+        success = self.move_base.wait_for_result(rospy.Duration(15))
+        return success
 
     def rotate(self, action):
         pass
@@ -73,9 +107,19 @@ class Logikoma:
     def update_pose(self,msg):
         self.pose = msg.pose.pose
 
+    def on_shutdown(self):
+        #save action history to file
+        print "Ending"
+
     def run(self):
         rospy.Subscriber(self.goal_topic, action_output, callback=self.process)
         rospy.Subscriber('/odom', Odometry, callback=self.update_pose)
+        rospy.on_shutdown(self.on_shutdown)
+        rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+
 
 
 if __name__ == '__main__':
