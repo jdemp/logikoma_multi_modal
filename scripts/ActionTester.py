5809b27d90167f334c641935df807771a0a28531
Jensen Dempsey <jensen.dempsey@colorado.edu>
2017-04-27 12:50:30 -0600

    fixed parsing issue

--- scripts/ActionTester.py
+++ scripts/ActionTester.py
@@ -4,19 +4,19 @@ from logikoma_multi_modal.msg import action_output
 
 class ActionTester:
     def __init__(self):
-        self.valid_actions = {'go forward':'go forward', 'continue':'continue', 'turn left':'turn left',
+        self.valid_actions = {'go straight':'go straight', 'continue':'continue', 'turn left':'turn left',
                               'turn right':'turn right'}
         self.goal_topic = '/user_goal'
         self.action_pub = rospy.Publisher(self.goal_topic, action_output, queue_size=1)
 
     def start(self):
         while not rospy.is_shutdown():
-            action = input("Enter an action")
+            action = raw_input("Enter an action ")
             if action in self.valid_actions.keys():
                 msg = action_output()
                 msg.action = action
                 msg.header.stamp = rospy.get_rostime()
-                self.goal_pub.publish(msg)
+                self.action_pub.publish(msg)
             else:
                 print "Not a valid action"
 
