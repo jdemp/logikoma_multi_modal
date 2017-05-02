d1f59de9db78542c7ff9196785ead49f9767743a
Jensen Dempsey <jdemp@jensendempsey.com>
2017-05-01 18:36:57 -0600

    fixed action tester

--- scripts/ActionTester.py
+++ scripts/ActionTester.py
@@ -4,11 +4,6 @@ from logikoma_multi_modal.msg import action_output
 
 class ActionTester:
     def __init__(self):
-        #self.valid_actions = {'go straight':'go straight', 'continue':'continue', 'turn left':'turn left',
-                              'turn right':'turn right', 'go back': 'go back', 'rotate left': 'rotate:left:90',
-                              'rotate right': 'rotate:right:90', 'turn around': 'rotate:180:180', 'search':'search',
-                              'hard right': 'hard right', 'hard left': 'hard left',
-                              'obstacle front': 'obstacle front', 'obstacle left':'obstacle left', 'obstacle right':'obstacle right'}
         self.goal_topic = '/user_goal'
         self.action_pub = rospy.Publisher(self.goal_topic, action_output, queue_size=1)
 
